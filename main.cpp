d8b6f6c6fd17c795ccac009a6640c43788adb13e
justin dzida <jsutin.dzida@teleplan.com>
2018-09-11 09:50:11 -0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <QCoreApplication>
+#include <QNetworkInterface>
+#include <QDebug>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QList<QNetworkInterface> interface = QNetworkInterface::allInterfaces();
+
+
+    QList<QNetworkAddressEntry> entries = interface.at(1).addressEntries();
+
+    QString ad = entries.at(0).ip().toString();
+    std::string ips = ad.toUtf8().constData();
+
+    std::cout << ips << std::endl;
+
+    ips.erase(std::remove(ips.begin(), ips.end(), '.'), ips.end());
+
+    uint32_t val = std::stoi(ips);
+
+    std::cout << val << std::endl;
+
+
+
+    return a.exec();
+}
